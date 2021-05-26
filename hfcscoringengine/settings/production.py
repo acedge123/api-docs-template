872dc9cab938f6ec7f83b41e1c73e85231a20295
TGA <tech@thegig.agency>
2021-05-26 16:51:31 +0000

    production changes

--- hfcscoringengine/settings/production.py
+++ hfcscoringengine/settings/production.py
@@ -87,19 +87,6 @@ STATIC_URL = f"https://{aws_s3_domain}/static/"
 DEFAULT_FILE_STORAGE = "scoringengine.utils.storages.MediaRootS3Boto3Storage"
 MEDIA_URL = f"https://{aws_s3_domain}/media/"
 
-# TEMPLATES
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#templates
-TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
-    (
-        "django.template.loaders.cached.Loader",
-        [
-            "django.template.loaders.filesystem.Loader",
-            "django.template.loaders.app_directories.Loader",
-        ],
-    )
-]
-
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
