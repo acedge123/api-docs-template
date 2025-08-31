dec6091c4ddfb9f3f02d1b94271c5a2ccf1c8b64
Alan Edgett <alan@thegig.agency>
2025-08-31 15:49:46 -0700

    Simplify URLs: Remove Swagger configuration to debug import issues

--- hfcscoringengine/urls.py
+++ hfcscoringengine/urls.py
@@ -14,27 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.http import JsonResponse
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
 from scoringengine.admin import admin_site
-
-# Swagger/OpenAPI schema view
-schema_view = get_schema_view(
-    openapi.Info(
-        title="HFC Scoring Engine API",
-        default_version='v1',
-        description="API for lead scoring and qualification system",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 def meaningful_health_check(request):
     """Simple health check that always returns 200 OK"""
@@ -49,9 +32,4 @@ urlpatterns = [
     path("api/", include("api.urls", namespace="api")),
     path("health/", meaningful_health_check, name="health_check"),
     path("test/", test_view, name="test"),
-    
-    # API Documentation
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
