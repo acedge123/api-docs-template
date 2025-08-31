c58aa4164a9299f7153419effaa581f586a998bc
Alan Edgett <alan@thegig.agency>
2025-08-31 09:44:40 -0700

    Fix double BrowserRouter issue in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Documentation from './pages/Documentation';
 import PrivacyPolicy from './pages/PrivacyPolicy';
@@ -10,11 +10,13 @@ import NotFound from './pages/NotFound';
 import Login from './pages/Login';
 import Admin from './pages/Admin';
 
-const App = () => (
-  <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
+const App = () => {
+  console.log('App component rendering...');
+  
+  return (
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
       <Routes>
         {/* Public Routes */}
         <Route path='/' element={<Index />} />
@@ -27,8 +29,8 @@ const App = () => (
         {/* Catch all */}
         <Route path='*' element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
-  </TooltipProvider>
-);
+    </TooltipProvider>
+  );
+};
 
 export default App;
