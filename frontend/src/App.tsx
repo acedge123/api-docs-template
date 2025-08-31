ea6412c711a4c8cf0e852d9476d3e6a69fe1513c
Alan Edgett <alan@thegig.agency>
2025-08-30 21:28:58 -0700

    Fix Vercel build issues: remove React Query dependency and update TypeScript config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Documentation from './pages/Documentation';
@@ -11,36 +10,25 @@ import NotFound from './pages/NotFound';
 import Login from './pages/Login';
 import Admin from './pages/Admin';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          {/* Public Routes */}
-          <Route path='/' element={<Index />} />
-          <Route path='/documentation' element={<Documentation />} />
-          <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-          <Route path='/terms-of-service' element={<TermsOfService />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/admin' element={<Admin />} />
-          
-          {/* Catch all */}
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path='/' element={<Index />} />
+        <Route path='/documentation' element={<Documentation />} />
+        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+        <Route path='/terms-of-service' element={<TermsOfService />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/admin' element={<Admin />} />
+        
+        {/* Catch all */}
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </TooltipProvider>
 );
 
 export default App;
